91352f5f60bcafcd107aa7ea19ffd72d72130eab
oFearr <fearadinopvp@hotmail.com>
2020-09-07 13:21:35 +0100

    Release version 1.3

--- src/main/java/me/ofearr/activity_rewards/Activity_Rewards.java
+++ src/main/java/me/ofearr/activity_rewards/Activity_Rewards.java
@@ -2,7 +2,6 @@ package me.ofearr.activity_rewards;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -10,7 +9,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.entity.EntityBreedEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -19,13 +17,22 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public final class Activity_Rewards extends JavaPlugin implements Listener {
 
+    public static Activity_Rewards plugin;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        plugin = this;
         loadconfig();
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        if(Bukkit.getServer().getVersion().contains("1.16")){
+            String SV = Bukkit.getServer().getVersion();
+            System.out.println("[ActivityRewards] > Detected server version " + SV + ", enabling additional listeners!");
+            Bukkit.getServer().getPluginManager().registerEvents(new Breeding(), this);
+        }
 
     }
+
 
     private void loadconfig() {
         saveDefaultConfig();
@@ -46,6 +53,7 @@ public final class Activity_Rewards extends JavaPlugin implements Listener {
                 for(int i = 0; i < 100; i++){
                     Double chance = getConfig().getDouble("mining." + e.getBlock().getType().toString() + "." + i + ".chance");
                     if (ThreadLocalRandom.current().nextDouble() <= chance) {
+                        System.out.println("[ActivityRewards] > [Logger] Dispatching mining reward commands for " + player.getName() + "!");
                         List<String> commands = getConfig().getStringList("mining." + e.getBlock().getType().toString() + "." + i + ".commands");
                         for(int c = 0; c < commands.size(); c++){
                             String UCMD = commands.get(c).replace("<player>", player.getName());
@@ -71,6 +79,7 @@ public final class Activity_Rewards extends JavaPlugin implements Listener {
             for (int i = 0; i < 100; i++) {
                 Double chance = getConfig().getDouble("slaying." + e.getEntity().getType().toString() + "." + i + ".chance");
                 if (ThreadLocalRandom.current().nextDouble() <= chance) {
+                    System.out.println("[ActivityRewards] > [Logger] Dispatching slaying reward commands for " + player.getName() + "!");
                     List<String> commands = getConfig().getStringList("slaying." + e.getEntity().getType().toString() + "." + i + ".commands");
                     for (int c = 0; c < commands.size(); c++) {
                         String UCMD = commands.get(c).replace("<player>", player.getName());
@@ -95,7 +104,7 @@ public final class Activity_Rewards extends JavaPlugin implements Listener {
                 } else {
                     reloadConfig();
                     player.sendMessage(ChatColor.GREEN + "Config successfully reloaded!");
-                    System.out.println(ChatColor.GREEN + "[ActivityRewards] " + player.getName() + " successfully reloaded config!");
+                    System.out.println(ChatColor.GREEN + "[ActivityRewards] > [Logger]" + player.getName() + " successfully reloaded config!");
                 }
             }
         }
@@ -108,7 +117,6 @@ public final class Activity_Rewards extends JavaPlugin implements Listener {
     public void Farm(BlockBreakEvent e){
         Player player = e.getPlayer();
         Block crop = e.getBlock();
-
         if(getConfig().getString("farming." + crop.getType().toString()) == null) {
             return;
         } else {
